3e8bac18944afdbc5ad0a852e327a90af7c10e5e
Raul <raulcg1987@gmail.com>
2020-04-21 18:12:11 +0100

    5 commit

--- PEC3App/app/src/main/java/edu/uoc/android/SettingsActivity.kt
+++ PEC3App/app/src/main/java/edu/uoc/android/SettingsActivity.kt
@@ -1,29 +1,35 @@
 package edu.uoc.android
 
 import android.Manifest
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.media.Image
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.provider.MediaStore.Images.Media.getBitmap
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_settings.*
+import kotlinx.android.synthetic.main.elemento_quizzes.*
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 
 
 class SettingsActivity : AppCompatActivity() {
 
-    val REQUEST_IMAGE_CAPTURE = 1
+    var REQUEST_IMAGE_CAPTURE = 1
     lateinit var currentPhotoPath: String
     val REQUEST_TAKE_PHOTO = 1
+    private val filepath = "UOCImageApp"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,35 +48,40 @@ class SettingsActivity : AppCompatActivity() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             // Ensure that there's a camera activity to handle the intent
             takePictureIntent.resolveActivity(packageManager)?.also {
-                // Create the File where the photo should go
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    // Error occurred while creating the File
-                    null
-                }
-                // Continue only if the File was successfully created
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                        this,
-                        "com.example.android.fileprovider",
-                        it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+
                     startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
                 }
             }
-        }
+
 
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                val imageBitmap = data!!.extras?.get("data") as Bitmap
+                imageView.setImageBitmap(imageBitmap)
 
 
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            val imageBitmap = data?.extras?.get("data") as Bitmap
-//            imageView.setImageBitmap(imageBitmap)
-//        }
+                var fileName = "imageapp.jpg"
+                var myExternalFile:File = File(getExternalFilesDir(filepath),fileName)
+                try {
+                    val stream = ByteArrayOutputStream()
+                    imageBitmap.compress(Bitmap.CompressFormat.JPEG,100,stream)
+                    val fileOutPutStream = FileOutputStream(myExternalFile)
+                    fileOutPutStream.write(stream.toByteArray())
+                    fileOutPutStream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                Toast.makeText(applicationContext,"data save in" + myExternalFile.path, Toast.LENGTH_SHORT).show()
+            } catch (ex: Error) {}
+
+        }
+
+
+
     }
 
 
@@ -93,7 +104,7 @@ class SettingsActivity : AppCompatActivity() {
             } else {
                 ActivityCompat.requestPermissions(
                     this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    225
+                    1888
                 )
             }
         }
